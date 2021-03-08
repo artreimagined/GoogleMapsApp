bbefabb0ccf848bd293926c07e853fe1c11c2429
artreimagined <teri.t.davis@gmail.com>
2021-03-08 13:49:13 -0500

    updates

--- src/main/java/com/tts/MapsApp/controller/MapController.java
+++ src/main/java/com/tts/MapsApp/controller/MapController.java
@@ -3,8 +3,6 @@ package com.tts.MapsApp.controller;
 import com.tts.MapsApp.model.Location;
 import com.tts.MapsApp.service.MapService;
 import lombok.extern.slf4j.Slf4j;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -18,20 +16,18 @@ public class MapController {
     @Autowired
     MapService mapService;
 
-    private Logger logger = LoggerFactory.getLogger(MapController.class);
-
     public MapController(MapService mapService) {
         this.mapService = mapService;
     }
 
-    @GetMapping("/home")
+    @GetMapping("/")
     public String getDefaultMap(Model model) {
-        Location location = new Location();
-        location.setCity("Atlanta");
-        location.setState("Georgia");
-        mapService.addCoordinates(location);
-        System.out.println(location);
-        log.info("This is my location: {}", location);
+//        Location location = new Location();
+//        location.setCity("Atlanta");
+//        location.setState("Georgia");
+//        mapService.addCoordinates(location);
+//        System.out.println(location);
+//        log.info("This is my location: {}", location);
         model.addAttribute(new Location());
         return "index.html";
     }
--- src/main/resources/static/custom.js
+++ src/main/resources/static/custom.js
@@ -19,11 +19,11 @@ function initMap() {
        animation: google.maps.Animation.BOUNCE
    });
 
-   let contentString = '<h2>' + city + ', ' + state + '</h2>';
+   let contentString = `<h2>${city}, ${state}</h2>`;
 
     let infowindow = new google.maps.InfoWindow({
       content: contentString
-    });
+    })
 
     google.maps.event.addListener(marker, 'click', () => infowindow.open(map,marker));
 }
